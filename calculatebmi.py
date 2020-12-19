2945f5f019f279a8c60edbb84fe7db681ef26722
Vadzim <omoegod@icloud.com>
2020-12-19 14:22:03 +0300

    change to specification

--- calculatebmi.py
+++ calculatebmi.py
@@ -8,13 +8,13 @@ scale2 = ("=" * (40 - IndexBMI) + "40")
 point = "|"
 
 if IndexBMI <= 18:
-    print('Ваш вес ниже нормы: ' + str(IndexBMI))
+    print('Ваш индекс массы тела ниже нормы: ' + str(IndexBMI))
     print(scale + point +  scale2)
 elif IndexBMI <= 25:
-    print('Ваш вес в норме: ' + str(IndexBMI))
+    print('Ваш индекс массы тела в норме: ' + str(IndexBMI))
     print(scale + point +  scale2)
 elif IndexBMI <= 40:
-    print('У Вас избыточный вес, пора бы заняться собой: ' + str(IndexBMI))
+    print('Ваш индекс массы тела выше нормы, пора бы заняться собой: ' + str(IndexBMI))
     print(scale + point +  scale2)        
 else:
     print('Возможно вы не смогли встать что бы ввести данные')
